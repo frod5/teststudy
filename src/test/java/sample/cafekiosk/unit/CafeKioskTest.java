2e5f15b1abf1f7ea7f61ad4f696d294b023135c4
pgm1976 <pgm1976@gepplus.co.kr>
2023-06-20 17:32:43 +0900

    Practical testing 가이드 학습 - JUnit5로 자동화 테스트

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,14 +1,17 @@
 package sample.cafekiosk.unit;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
+import sample.cafekiosk.unit.beverage.Latte;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CafeKioskTest {
 
     @Test
-    void add() {
+    void add_manual_test() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
@@ -16,6 +19,46 @@ class CafeKioskTest {
         //무조건 성공하는 코드
         System.out.println(">>> 담긴 음료 수 : "+ cafeKiosk.getBeverages().size());
         System.out.println(">>> 담긴 음료 : "+ cafeKiosk.getBeverages().get(0).getName());
+    }
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        //자동화 테스트
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        //추가
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        //삭제
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        //추가
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+
+        //클리어
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
 }
