24144d309da24987e820098a15f380f5933607f1
pgm1976 <pgm1976@gepplus.co.kr>
2023-06-29 16:35:11 +0900

    Practical testing 가이드 학습 - Business Layer (product service) TDD

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -3,6 +3,10 @@ package sample.cafekiosk.spring.api.service.product;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
@@ -12,6 +16,7 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -21,5 +26,29 @@ public class ProductService {
                 .stream()
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    //동시성 이슈
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProduct();
+        Product product = request.toEntity(nextProductNumber);
+
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
+    private String createNextProduct() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        if(!StringUtils.hasText(latestProductNumber)) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.valueOf(latestProductNumber);
+        int nextProductNumber = latestProductNumberInt + 1;
+
+        return String.format("%03d",nextProductNumber);
     }
 }
